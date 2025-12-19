7b822669a41fafe46d01859803015e6c40050339
Kiet <31864905+Kitenite@users.noreply.github.com>
2025-12-18 16:20:33 -0800

    Refactor desktop types + remove ipc channels (#423)
    * update .env.example and refactor types
    
    * remove types
    
    * remove unused ipc channels

---
+++ apps/desktop/src/shared/types/index.ts
@@ -0,0 +1,7 @@
+export * from "./config";
+export * from "./database";
+export * from "./electron";
+export * from "./mosaic";
+export * from "./tab";
+export * from "./workspace";
+export * from "./worktree";
